4693f08f8fcd6396a4741f54ad63b06a04e0af44
temuET <ttemu2016@gmail.com>
2022-06-30 18:38:20 +0000

    godd work

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+	/**
+	*main - Entry point
+	*
+	*Return: Always 0 (success)
+	*/
 int main(void)
 {
-
-	char myStr[] = "Programming is like building a multilingual puzzle\n";
+	char myStr[] = "\"Programming is like building a multilingual puzzle\n";
 
 	puts(myStr);
 	return (0);
