cc5a67a223b89a7a412eba9536b1c0771936506a
temuET <ttemu2016@gmail.com>
2022-06-30 19:17:10 +0000

    write

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
-i#include <stdio.h>
-	/**
-	*main - Entry point
-	*
-	*Return: Always 0 (success)
-	*/
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: Always 0 (success)
+*/
 int main(void)
 {
-	/**
-	*This main function
-	*/
 	puts("\"Programming is like building a multilingual puzzle,\n");
 	return (0);
 }
