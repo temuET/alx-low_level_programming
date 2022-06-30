0819fcd138b9a8e56d857dacfb116093375fc3d6
temuET <ttemu2016@gmail.com>
2022-06-30 19:23:54 +0000

    Done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,8 @@
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	char myStr[] = "\"Programming is like building a multilingual puzzle\n";
+
+	puts(myStr);
 	return (0);
 }
