46cbc9fefdc062e66195490590dc952b20776b17
temuET <ttemu2016@gmail.com>
2022-07-06 23:17:37 +0000

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - entery point
+*Return: 0
+*/
+int main(void)
+{
+	int a, b, c, i;
+
+	a = 1;
+	b = 2;
+	for (i = 1; i <= 50; i++)
+	{
+		printf("%d, ", a);
+		c = a + b;
+		a = b;
+		b = c;
+	}
+	return (0);}
