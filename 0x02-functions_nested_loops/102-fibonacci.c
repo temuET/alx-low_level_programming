65707adf62eda9712b22741ba5977c674f40944e
temuET <ttemu2016@gmail.com>
2022-07-10 16:08:48 +0000

    fabonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,17 +5,20 @@
 */
 int main(void)
 {
-	int a, b, c, i;
+	int i;
+	unsigned long a, b, c;
 
 	a = 1;
 	b = 2;
-	for (i = 0; i < 50; i++)
+	for (i = 0; i < 50; ++i)
 	{
-		printf("%1d, ", a);
 		c = a + b;
+		printf("%lu, ", a);
 		a = b;
 		b = c;
+
 	}
 	printf("\n");
+
 	return (0);
 }
