7643989cc1829f9406850b3568b52c13e394caec
temuET <ttemu2016@gmail.com>
2022-07-12 02:27:28 +0000

    prin reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+*print_rev - print revers character
+*@s: string parm
+*/
+
+void print_rev(char *s)
+{
+	int x, i;
+
+	x = strlen(s);
+	for (i = x - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+}
