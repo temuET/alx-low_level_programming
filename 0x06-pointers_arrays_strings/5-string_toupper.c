75efc116d9d54ae4251671a734a64723587ef80f
temuET <ttemu2016@gmail.com>
2022-07-15 17:54:38 +0000

    hot fix

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,15 +8,14 @@
 
 char *string_toupper(char *s)
 {
-	int i;
+	int i = 0;
 
-	while (s[i] != '\0')
+	while (s[i++])
 	{
 		if (s[i] >= 97 && s[i] <= 122)
 		{
 			s[i] = s[i] - 32;
 		}
-		i++;
 	}
 	return (s);
 }
