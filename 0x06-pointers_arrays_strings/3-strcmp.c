ec502b035ce068686116af85a365d2557fc23536
temuET <ttemu2016@gmail.com>
2022-07-15 01:49:41 +0000

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,10 +5,14 @@
 * _strcmp - compare two things like s1 and s2
 * @s1: first arg
 * @s2: second arg
-* Return: string
+* Return: integer
 */
 
 int _strcmp(char *s1, char *s2)
 {
-	strcmp(s1, s2);
+	int result;
+
+	result = strcmp(s1, s2);
+
+	return (result);
 }
