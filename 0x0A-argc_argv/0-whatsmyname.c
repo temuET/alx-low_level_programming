d893a99a1c4739dd6fb071a841f9ce4641eff0b3
temuET <ttemu2016@gmail.com>
2022-07-23 02:08:47 +0000

    my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*main - entery point to the program
+*Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
