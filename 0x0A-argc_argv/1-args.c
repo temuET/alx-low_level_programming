631e89ae3750d59c1e882245b47c5505524ad19e
temuET <ttemu2016@gmail.com>
2022-07-23 02:53:50 +0000

    printing argc with the new line

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*main - entry point
+*@argc: arg counter
+*@argv: arg vectot
+*Return: 0
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
