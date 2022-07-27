a4c3f1e40f00442a6e0eb5655f59f2bc3f726f1f
temuET <ttemu2016@gmail.com>
2022-07-27 15:08:33 +0000

    malloc function

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+*_strdup - retun a pointer to a new string
+*@str: str arg
+*Return: pointer
+*/
+
+char *_strdup(char *str)
+{
+	char *strp;
+	int i;
+	int count;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i] != '\0'; i++);
+
+	strp = malloc(i * sizeof(char) + 1);
+	if (strp == NULL)
+	{
+		return (NULL);
+	}
+	for (count = 0; count < i; count++)
+	{
+		strp[count] = str[count];
+	}
+	strp[count] = '\0';
+
+	return (strp);
+}
