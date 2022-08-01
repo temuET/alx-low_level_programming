59557b9e8708dc1abd254fd2abf1ceeb2cadb0db
temuET <ttemu2016@gmail.com>
2022-08-01 17:40:47 +0000

    struct defnition

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+*struct dog - strcture for dog character
+*@name: name
+*@age: age
+*@owner: owner
+*Description: describe the property
+*/
+
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
